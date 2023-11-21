3c4a63ba7c09695b2d6a7d1e6e41558894941fb0
hyseh <edgyhyseh@gmail.com>
2023-11-21 20:55:36 +0100

    added api url and endpoints

---
+++ src/js/api/api.js
@@ -0,0 +1,5 @@
+export const BASE_URL = 'https://api.noroff.dev/api/v1';
+export const LOGIN_ENDPOINT = '/auction/auth/login';
+export const REGISTER_ENDPOINT = '/auction/auth/register';
+export const LISTINGS_ENDPOINT = '/auction/listings/';
+export const PROFILES_ENDPOINT = '/auction/profiles/';
