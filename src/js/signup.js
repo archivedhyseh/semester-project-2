811584081c83b3ae10fa43bb82b8093e53435bcc
hyseh <edgyhyseh@gmail.com>
2023-11-24 08:23:02 +0100

    added signup functionality

---
+++ src/js/signup.js
@@ -0,0 +1,78 @@
+import { login } from './api/auth/login.js';
+import { register } from './api/auth/register.js';
+
+const signupName = document.querySelector('#signup-name');
+const signupEmail = document.querySelector('#signup-email');
+const signupPassword = document.querySelector('#signup-password');
+const signupMessage = document.querySelector('#signup-message');
+const signupButton = document.querySelector('#signup-button');
+
+signupButton.addEventListener('click', async (e) => {
+  e.preventDefault();
+  const name = signupName.value;
+  const email = signupEmail.value;
+  const password = signupPassword.value;
+  const data = await register(name, email, password);
+  signupValidation(data);
+});
+
+async function signupValidation(data) {
+  const signupNameError = document.querySelector('#signup-name-error');
+  const signupEmailError = document.querySelector('#signup-email-error');
+  const signupPasswordError = document.querySelector('#signup-password-error');
+
+  signupNameError.classList.add('hidden');
+  signupNameError.innerHTML = '';
+  signupEmailError.classList.add('hidden');
+  signupEmailError.innerHTML = '';
+  signupPasswordError.classList.add('hidden');
+  signupPasswordError.innerHTML = '';
+  signupMessage.classList.add('hidden');
+  signupMessage.innerHTML = '';
+
+  if (data.errors) {
+    for (let i = 0; i < data.errors.length; i++) {
+      if (data.errors[0].message === 'Profile already exists') {
+        const message = 'Account already exists.';
+        signupMessage.classList.remove('hidden');
+        signupMessage.innerHTML = `<p>${message}</p>`;
+      } else {
+        if (data.errors[i].path[0] === 'name') {
+          const message =
+            'Names can only use letters, numbers and underscores.';
+          signupNameError.classList.remove('hidden');
+          signupNameError.innerHTML = `<p>${message}</p>`;
+        }
+
+        if (data.errors[i].path[0] === 'email') {
+          const message = 'Only @noroff.no emails are allowed to register.';
+          signupEmailError.classList.remove('hidden');
+          signupEmailError.innerHTML = `<p>${message}</p>`;
+        }
+
+        if (data.errors[i].path[0] === 'password') {
+          const message = 'Password must be at least 8 characters long.';
+          signupPasswordError.classList.remove('hidden');
+          signupPasswordError.innerHTML = `<p>${message}</p>`;
+        }
+      }
+    }
+  } else {
+    const email = signupEmail.value;
+    const password = signupPassword.value;
+    const data = await login(email, password);
+    autoLogin(data);
+  }
+}
+
+async function autoLogin(data) {
+  if (data.errors) {
+    const message = data.errors[0].message;
+    signupMessage.classList.remove('hidden');
+    signupMessage.innerHTML = `<p>${message}</p>`;
+  } else {
+    localStorage.setItem('name', data.name);
+    localStorage.setItem('token', data.accessToken);
+    setTimeout((window.location = './index.html'), 100);
+  }
+}
