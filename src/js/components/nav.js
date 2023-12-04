72b3bfff30375ae6a0e54216cb5cda0ec167d850
hyseh <edgyhyseh@gmail.com>
2023-12-04 11:58:00 +0100

    updated variables names and removed commented lines

--- src/js/components/nav.js
+++ src/js/components/nav.js
@@ -1,19 +1,16 @@
 const navBtn = document.querySelector('#navigation-button');
 const navMenu = document.querySelector('#navigation-menu');
 
-let menuActive = false;
+let navMenuOpen = false;
 
 navBtn.addEventListener('click', () => {
-  // const main = document.querySelector('main');
-  if (!menuActive) {
-    // main.setAttribute('aria-hidden', 'true');
+  if (!navMenuOpen) {
     navBtn.setAttribute('aria-expanded', 'true');
     navMenu.classList.toggle('hidden');
-    menuActive = true;
+    navMenuOpen = true;
   } else {
-    // main.removeAttribute('aria-hidden');
     navBtn.setAttribute('aria-expanded', 'false');
     navMenu.classList.toggle('hidden');
-    menuActive = false;
+    navMenuOpen = false;
   }
 });
