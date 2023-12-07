e92eae8be64fafdaa71e0b0d9edf15141ddcec6c
hyseh <edgyhyseh@gmail.com>
2023-12-07 11:29:50 +0100

    updated getListings parameters and added getListing function

--- src/js/api/listings/listings.js
+++ src/js/api/listings/listings.js
@@ -1,8 +1,20 @@
 import { BASE_URL, LISTINGS_ENDPOINT } from '../api.js';
 
 export async function getListings(limit, offset, sort, active, tag) {
-  const param = `?limit=${limit}&offset=${offset}&sortOrder=${sort}&_active=${active}&_tag=${tag}&_bids=true`;
+  const param = `?limit=${limit}&offset=${offset}&sortOrder=${sort}&_active=${active}&_tag=${tag}&_bids=true&sort=created`;
   const res = await fetch(`${BASE_URL}${LISTINGS_ENDPOINT}${param}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  const data = await res.json();
+  return data;
+}
+
+export async function getListing(id) {
+  const param = `?_seller=true&_bids=true`;
+  const res = await fetch(`${BASE_URL}${LISTINGS_ENDPOINT}${id}${param}`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
