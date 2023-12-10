e0024830d7ef2ca5ea95e7f02fcef32c2c19a081
hyseh <edgyhyseh@gmail.com>
2023-12-10 22:19:05 +0100

    fixed navbar and updated nav component

--- src/js/components/nav.js
+++ src/js/components/nav.js
@@ -14,3 +14,53 @@ navBtn.addEventListener('click', () => {
     navMenuOpen = false;
   }
 });
+
+const navLoad = () => {
+  const token = localStorage.getItem('token');
+
+  if (token) {
+    console.log('token found');
+    primaryNavigation();
+    secondaryNavigation();
+  } else {
+    console.log('no token');
+  }
+};
+
+function primaryNavigation() {
+  const container = document.querySelector('#primary-navigation');
+
+  container.innerHTML = `
+    <a href="./index.html" class="p-3 md:py-2">Home</a>
+    <a href="./explore.html" class="p-3 md:py-2">Explore</a>
+    <a href="./create.html" class="p-3 md:py-2">Create</a>
+    `;
+}
+
+function secondaryNavigation() {
+  const container = document.querySelector('#secondary-navigation');
+  container.innerHTML = '';
+
+  const profile = document.createElement('a');
+  profile.href = './profile.html';
+  profile.classList.add('p-3', 'md:py-2');
+  profile.innerText = 'Profile';
+
+  const logout = document.createElement('button');
+  logout.classList.add('p-3', 'text-left', 'md:py-2', 'md:text-center');
+  logout.innerText = 'Sign out';
+  logout.addEventListener('click', () => {
+    localStorage.removeItem('token');
+    console.log('removed token');
+    setTimeout(() => {
+      window.location = './index.html';
+    }, 100);
+  });
+
+  container.appendChild(profile);
+  container.appendChild(logout);
+
+  return container;
+}
+
+navLoad();
