982f3bc2edc97d8b884748d1e255e0281ad6906c
hyseh <edgyhyseh@gmail.com>
2023-12-12 14:30:50 +0100

    added postListing function

---
+++ src/js/api/listings/create.js
@@ -0,0 +1,15 @@
+import { BASE_URL, LISTINGS_ENDPOINT } from '../api.js';
+
+export async function postListing(title, description, tags, media, endsAt) {
+  const token = localStorage.getItem('token');
+  const res = await fetch(`${BASE_URL}${LISTINGS_ENDPOINT}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ title, description, tags, media, endsAt }),
+  });
+  const data = await res.json();
+  return data;
+}
