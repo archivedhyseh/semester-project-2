b208f8e866547e7c87575172d210f8c766d55a29
hyseh <edgyhyseh@gmail.com>
2023-12-13 15:06:50 +0100

    added putAvatar function

---
+++ src/js/api/profiles/avatar.js
@@ -0,0 +1,15 @@
+import { BASE_URL, PROFILES_ENDPOINT } from '../api.js';
+
+export async function putAvatar(name, avatar) {
+  const token = localStorage.getItem('token');
+  const res = await fetch(`${BASE_URL}${PROFILES_ENDPOINT}${name}/media`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ avatar }),
+  });
+  const data = await res.json();
+  return data;
+}
