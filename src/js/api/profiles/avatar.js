693c182111fdd2c4d1c45d3301bcead0adb00521
hyseh <edgyhyseh@gmail.com>
2023-12-16 17:03:50 +0100

    added try catch blocks to functions

--- src/js/api/profiles/avatar.js
+++ src/js/api/profiles/avatar.js
@@ -2,14 +2,18 @@ import { BASE_URL, PROFILES_ENDPOINT } from '../api.js';
 
 export async function putAvatar(name, avatar) {
   const token = localStorage.getItem('token');
-  const res = await fetch(`${BASE_URL}${PROFILES_ENDPOINT}${name}/media`, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${token}`,
-    },
-    body: JSON.stringify({ avatar }),
-  });
-  const data = await res.json();
-  return data;
+  try {
+    const res = await fetch(`${BASE_URL}${PROFILES_ENDPOINT}${name}/media`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({ avatar }),
+    });
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
 }
